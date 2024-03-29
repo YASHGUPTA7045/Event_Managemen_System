da4b67a81b0b66db71cf968bc318f3b64329e0b1
YASHGUPTA7045 <yashgupta7045@gmail.com>
2024-03-30 01:56:57 +0530

    hosting on vercel.com

--- EventManagementSystem/urls.py
+++ EventManagementSystem/urls.py
@@ -41,3 +41,5 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += re_path(r'.+', never_cache(serve_static)),
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
